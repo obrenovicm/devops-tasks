8908a3f704e9624b5ceddcc2273181118b0bb0aa
Milos Obrenovic <mobrenovic@griddynamics.com>
2025-03-19 15:07:01 +0100

    [update] ansible; run script

--- ansible-task/tf/run.sh
+++ ansible-task/tf/run.sh
@@ -6,4 +6,4 @@ terraform init
 terraform plan
 terraform apply -auto-approve
 
-ansible-playbook -i ../ansible/inventory.gcp.yml --private-key="~/.ssh/ansible_ed25519" ../ansible/playbook.yml
+ansible-playbook -i ../ansible/inventory.gcp.yml ../ansible/playbook.yml
